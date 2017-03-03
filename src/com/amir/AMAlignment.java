9d2d8199226601a669c45b4884db0684ee876f44
Amir <amir.atashin@gmail.com>
2017-03-03 13:31:27 +0330

    first good result!

--- src/com/amir/AMAlignment.java
+++ src/com/amir/AMAlignment.java
@@ -7,26 +7,36 @@
 package com.amir;
 
 import fr.inrialpes.exmo.align.impl.DistanceAlignment;
-import fr.inrialpes.exmo.align.impl.URIAlignment;
-import fr.inrialpes.exmo.align.impl.method.*;
 import fr.inrialpes.exmo.ontosim.string.StringDistances;
+import fr.inrialpes.exmo.ontosim.util.HungarianAlgorithm;
+import fr.inrialpes.exmo.ontowrap.HeavyLoadedOntology;
 import org.semanticweb.owl.align.Alignment;
-import org.semanticweb.owl.align.AlignmentProcess;
 import org.semanticweb.owl.align.AlignmentException;
+import org.semanticweb.owl.align.AlignmentProcess;
 
-import org.semanticweb.owl.align.Cell;
-
-import java.net.URI;
 import java.util.Properties;
 
 public class AMAlignment extends DistanceAlignment implements AlignmentProcess {
 
+    private HeavyLoadedOntology<Object> heavyOntology1;
+    private HeavyLoadedOntology<Object> heavyOntology2;
     public AMAlignment() {
+        heavyOntology1 = heavyOntology2 = null;
+        setType("**");
+    }
+
+    public void init(Object o1, Object o2, Object ontologies) throws AlignmentException {
+        super.init( o1, o2, ontologies );
+        if ( !( getOntologyObject1() instanceof HeavyLoadedOntology
+                && getOntologyObject1() instanceof HeavyLoadedOntology ))
+            throw new AlignmentException( "StrucSubsDistAlignment requires HeavyLoadedOntology ontology loader" );
     }
     @SuppressWarnings("Duplicates")
     public void align(Alignment alignment, Properties param) throws AlignmentException {
         try {
+
 /*
+            // *** Method 1 ***
             URI url1 = getOntology1URI();
             URI url2 = getOntology2URI();
 
@@ -46,7 +56,9 @@ public class AMAlignment extends DistanceAlignment implements AlignmentProcess {
             }
 */
 
-            double[][] matrix1 = new double[ontology1().nbClasses()][ontology2().nbClasses()];
+/*
+            // *** Method 2 ***
+            double[][] matrix = new double[ontology1().nbClasses()][ontology2().nbClasses()];
             double ii = 0;
             int jj = -1;
             int nbClass1 = ontology2().nbClasses();
@@ -61,27 +73,55 @@ public class AMAlignment extends DistanceAlignment implements AlignmentProcess {
                 }
                 if((ii += step) >= (jj + 1)) System.out.print(String.format("\r%d%% completed!", ++jj));
             }
+*/
+            heavyOntology1 = (HeavyLoadedOntology<Object>)getOntologyObject1();
+            heavyOntology2 = (HeavyLoadedOntology<Object>)getOntologyObject2();
 
-            int nbclasses1 = getOntologyObject1().nbClasses();
-            int nbclasses2 = getOntologyObject2().nbClasses();
-            double[][] matrix = new double[nbclasses1][nbclasses2];
-            Object[] class1 = new Object[nbclasses1];
-            Object[] class2 = new Object[nbclasses2];
+            int nbClasses1 = heavyOntology1.nbClasses();
+            int nbClasses2 = heavyOntology2.nbClasses();
+
+            double[][] matrix = new double[nbClasses1][nbClasses2];
+            Object[] class1o = new Object[nbClasses1];
+            Object[] class2o = new Object[nbClasses2];
+            String[] class1s = new String[nbClasses1];
+            String[] class2s = new String[nbClasses2];
             int i = 0;
+            String str1, str2;
+
             for ( Object ob : getOntologyObject1().getClasses() ) {
-                class1[i++] = ob;
-            }
-            i = 0;
-            for ( Object ob : getOntologyObject2().getClasses() ) {
-                class2[i++] = ob;
+                str1 = heavyOntology1.getEntityAnnotations(ob).iterator().next();
+                //str2 = heavyOntology1.getEntityName(ob);
+                class1s[i] = str1.trim().toLowerCase();
+                class1o[i++] = ob;
             }
 
-            double threshold = 0.5;
-            int[][] result = callHungarianMethod( matrix, nbclasses1, nbclasses2 );
+            int j = 0;
+            for ( Object ob : getOntologyObject2().getClasses() ) {
+                str1 = heavyOntology2.getEntityAnnotations(ob).iterator().next();
+                //str2 = heavyOntology2.getEntityName(ob);
+                class2s[j] = str1.trim().toLowerCase();
+                class2o[j++] = ob;
+            }
+
+            double ii = 0;
+            int jj = -1;
+            double step = 100.0 / nbClasses1;
+
+            for( i=0; i < nbClasses1; ++i ) {
+                for (j = 0; j < nbClasses2; ++j) {
+                    matrix[i][j] = 1.0 - StringDistances.levenshteinDistance(class1s[i], class2s[j]);
+                }
+                if((ii += step) >= (jj + 1)) System.out.print(String.format("\r%d%% completed!", ++jj));
+            }
+            double threshold = 0.3;
+            int[][] result = HungarianAlgorithm.hgAlgorithm( matrix, "max" );
+            //int[][] result = callHungarianMethod( matrix, nbClasses1, nbClasses2 );
+
+            System.out.println("OK");
             for( i=0; i < result.length ; i++ ){
-                double val = 1 - stringDistances.equalDistance(class1[result[i][0]].toString(),class2[result[i][1]].toString());
+                double val = matrix[result[i][0]][result[i][1]];
                 if( val > threshold ){
-                    addAlignCell(class1[result[i][0]], class2[result[i][1]], "=", val );
+                    addAlignCell(class1o[result[i][0]], class2o[result[i][1]], "=", val );
                 }
             }
 
